1f999a6a00d050697b4c8fdb0885907dbf8ca561
omid.zahed <omid.zahed@snapp.cab>
2023-05-24 17:32:37 +0330

    tags added

--- error.go
+++ error.go
@@ -7,4 +7,8 @@ var (
 	ErrStdOut = errors.New("unable to use stdout")
 	ErrNoArgs = errors.New("not enough arguments")
 	ErrNilCmd = errors.New("no command is provided")
+
+	// Tags
+	ErrNilPointer = errors.New("can not parse a nil value")
+	ErrNoPtr      = errors.New("can not parse non pointer")
 )
